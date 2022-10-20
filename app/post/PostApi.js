b0f1d8e1b6fd3b362691f9a76f07a4323fd47949
Naim Lawani <naimlawani01@gmail.com>
2022-10-20 16:30:10 +0200

    Debut route pour les posts

---
+++ app/post/PostApi.js
@@ -0,0 +1,27 @@
+import express from 'express';
+const postRouter = express.Router()
+
+//Recuperer tous les posts 
+postRouter.get('/', (req, res) => {
+
+})
+
+//Recuperer tous les posts pour un utilisateur
+postRouter.get('/:userId', (req, res) => {
+
+})
+
+//Creer un post (Ajouter un post )
+postRouter.post('/', (req, res) => {
+
+})
+
+//Modifier  un post 
+postRouter.put('/:id', (req, res) => {
+
+})
+
+//Supprimer un poste 
+postRouter.delete('/', (req, res) => {
+
+})
